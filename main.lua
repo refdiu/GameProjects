8a3824910eecf7aa5f52c310e4d45f954eb1159f
refdiu <lahirishraman@gmail.com>
2024-08-05 12:44:05 +0530

    added OOP objects

--- main.lua
+++ main.lua
@@ -1,7 +1,20 @@
 --program to implement infinite scrolling of background
-love.window.setFullscreen(true, "desktop")
+require('back_g')
 
-bg = dofile("back_g.lua")
+function love.load()
+	love.window.setMode(640, 480, {fullscreen = false, vsync = -1, resizable = false, centered = true})
+	bg = Background
+	bg:render()
+end
+
+function love.draw()
+	bg:draw()
+end
+
+function love.update(dt)
+	bg:u(dt)
+end
+
 
 function love.keypressed(key)
 	if key == "escape" then
