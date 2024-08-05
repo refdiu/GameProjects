c7cd8b7d0695542dcae87afeeed96d72f10e230c
refdiu <lahirishraman@gmail.com>
2024-08-05 20:53:35 +0530

    Added tilemaps

--- assets/world/back_g.lua
+++ assets/world/back_g.lua
@@ -1,25 +1,25 @@
 --program to implement infinite scrolling of background
-Background = {pic_x1 = 1366, pic_x2 = 0, pic_y = 0, image = "bg.jpeg"}
+Background = {pic_x1 = 1366, pic_x2 = 0, pic_y = 0, image = "assets/world/bg1.jpeg"}
 
-function Bg:new()
+function Background:new()
 	o = o or {}
 	setmetatable(o, self)
 	self.__index = self
 	return o
 end
 
-function Bg:render()
+function Background:render()
 	renimg = love.graphics.newImage(self.image)
 end
 
 
 
-function Bg:draw()
+function Background:drawim()
 	love.graphics.draw(renimg, -self.pic_x1, self.pic_y)
 	love.graphics.draw(renimg, self.pic_x2, self.pic_y)
 end
 
-function Bg:u(tpassed)
+function Background:u(tpassed)
 	self.pic_x1 = self.pic_x1 + tpassed*90
 	if self.pic_x1 >= 1366 then
 		self.pic_x1 = -1366
--- main.lua
+++ main.lua
@@ -1,6 +1,14 @@
 --program to implement infinite scrolling of background
-require('back_g')
-
+require('assets/world/back_g')
+sti = require('sti')
+platformlvl = {sti('assets/world/lvl1.lua'), 
+	sti('assets/world/lvl2.lua'), 
+	sti('assets/world/lvl3.lua'), 
+	sti('assets/world/lvl4.lua'),
+	sti('assets/world/lvl5-0.lua'),
+	sti('assets/world/lvl5-1.lua')
+}
+sel = 1
 function love.load()
 	love.window.setMode(640, 480, {fullscreen = false, vsync = -1, resizable = false, centered = true})
 	bg = Background
@@ -8,7 +16,8 @@ function love.load()
 end
 
 function love.draw()
-	bg:draw()
+	bg:drawim()	
+	platformlvl[sel]:draw(0, 400)
 end
 
 function love.update(dt)
@@ -19,5 +28,8 @@ end
 function love.keypressed(key)
 	if key == "escape" then
 		love.event.quit()
+	end
+	if key == "c" then
+		sel = math.random(1, 6)
 	end
 end
