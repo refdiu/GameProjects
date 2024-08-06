92cfb5c94aec13023d9c9418811eb141528ee157
refdiu <lahirishraman@gmail.com>
2024-08-07 00:03:14 +0530

    Translate assets

--- main.lua
+++ main.lua
@@ -1,36 +1,35 @@
---program to implement infinite scrolling of background
-require('assets/world/back_g')
-sti = require('sti')
-platformlvl = {sti('assets/world/lvl1.lua'), 
-	sti('assets/world/lvl2.lua'), 
-	sti('assets/world/lvl3.lua'), 
-	sti('assets/world/lvl4.lua'),
-	sti('assets/world/lvl5-0.lua'),
-	sti('assets/world/lvl5-1.lua')
-}
-sel = 1
+require('assets/world/coo')
 function love.load()
 	love.window.setMode(640, 480, {fullscreen = false, vsync = -1, resizable = false, centered = true})
 	love.window.setTitle('Road to Code')
-	bg = Background
-	bg:render()
+	love.graphics.setDefaultFilter('nearest', 'nearest')
+	c = coordinates
+	mx = 0
+	my = 0
+	game_state = nil
 end
 
 function love.draw()
-	bg:drawim()	
-	platformlvl[sel]:draw(0, 400)
+	love.graphics.print("Untitled Platformer Game", 120, 85, 0, 2.5, 2.5)
+	love.graphics.print("Start", 299, 200, 0, 1.7, 1.7)
+	love.graphics.print("Exit", 304, 300, 0, 1.7, 1.7)
+	love.graphics.print("CC0 (Public Domain)", 0, 460)
+	love.graphics.print("Made by Pitiful Games", 500, 460)
+	c:show(mx, my)
 end
 
-function love.update(dt)
-	bg:u(dt)
+function love.mousemoved(x, y)
+	mx = x
+	my = y
 end
 
-
-function love.keypressed(key)
-	if key == "escape" then
-		love.event.quit()
-	end
-	if key == "c" then
-		sel = math.random(1, 6)
+function love.mousepressed(px,py, button)
+	if button == 1 then	
+		if px >= 290 and px <= 360 and py >= 200 and py <= 250 then
+			dofile("game.lua")
+		end
+		if px >= 290 and px <= 360 and py >= 300 and py <= 350 then
+			love.event.quit()
+		end
 	end
 end
