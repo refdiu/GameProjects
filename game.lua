b3c6c513fa3fd4f699ff94171ad34a7076270543
refdiu <lahirishraman@gmail.com>
2024-08-08 01:07:32 +0530

    added jump attribute

--- game.lua
+++ game.lua
@@ -1,9 +1,13 @@
 camS = -0
 camsp = 50
-
+gravity = 7
+Jump_v = -250
+pDY = 0
+pY = 330
 
 require('assets/world/back_g')
 player = love.graphics.newImage("assets/world/player.jpeg")
+p_height = love.graphics.getWidth(player)
 bg = Background
 sti = require('stibg')
 platformlvl1 = sti('assets/world/lvl1.lua')
@@ -16,14 +20,17 @@ bg:render()
 
 
 function love.keypressed(key)
-	if key == "escape" then
+	if key == 'escape' then
 		love.event.quit()
 	end
-	if key == "c" then
+	if key == 'c' then
 		sel = math.random(1, 6)
 	end
-	if key == "r" then
+	if key == 'r' then
 		dofile("main.lua")
+	end
+	if key == 'space' and pDY == 0 then
+		pDY = Jump_v
 	end
 end
 
@@ -32,7 +39,7 @@ render_val = 0
 function love.draw()
 	ncamS = -math.floor(camS)
 	bg:drawim()
-	love.graphics.draw(player, 300, 330)
+	love.graphics.draw(player, 300, pY)
 	local pF = 0.27
 	love.graphics.print(ncamS, 0, 0)
 	if ncamS >= -640 and ncamS <= 0 then
@@ -48,6 +55,12 @@ end
 
 
 function love.update(dt)
+	pDY = pDY + gravity
+	pY = pY + pDY*dt
+	if pY > 330 then
+		pY = 330
+		pDY = 0
+	end
 	bg:u(dt)
 	if love.keyboard.isDown("left") then
 		camS = camS - camsp*dt
