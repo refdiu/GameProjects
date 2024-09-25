a0d9aa5ad4b2453e28c192dc85c0a6ac1686a33e
refdiu <lahirishraman@gmail.com>
2024-09-26 02:13:33 +0530

    can choose our ship

--- space_shooter/assets/utils.lua
+++ space_shooter/assets/utils.lua
@@ -1,13 +1,12 @@
-coordinates = {x, y}
+Coordinates = {}
 
-function coordinates:new()
+function Coordinates:new()
 	o = o or {}
 	setmetatable(o, self)
 	self.__index = self
 	return o
 end
 
-function coordinates:show(x, y)
-	love.graphics.print("x coordinate: "..x)
-	love.graphics.print('\ny coordinate: '..y)	
+function Coordinates:show(x, y)
+	love.graphics.print("x coordinate: "..x.."\ny coordinate: "..y)
 end
