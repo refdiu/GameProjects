c4b9b376f8af2c079ef5fc14f0cb261ec8405e18
refdiu <lahirishraman@gmail.com>
2024-12-15 09:20:51 +0530

    Whack-A-Mole Style Game

--- physics-based-rps/conf.lua
+++
@@ -1,4 +0,0 @@
-function love.conf(t)
-	t.version = "11.5"
-	t.console = false
-end
--- physics-based-rps/main.lua
+++
@@ -1,8 +0,0 @@
-function love.load()
-	love.graphics.setBackgroundColor(255, 255, 255)
-	clicked = nil
-end
-
-function love.draw()
-	if clicked then
-		love.graphics.draw(
