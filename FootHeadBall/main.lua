e01c437f50fb132046086a1e6aa05b1ad4711d30
refdiu <lahirishraman@gmail.com>
2025-02-25 15:17:27 +0530

    jumping logic to p1

--- FootHeadBall/main.lua
+++ FootHeadBall/main.lua
@@ -1,3 +1,6 @@
+local v = 360
+local u = 1.5
+
 function love.load()  
   player_1 = require('p1')
   player_1 = P1
@@ -6,12 +9,28 @@ end
 
 function love.draw()
   love.graphics.print("Hewll")
-  player_1:draw(0, 320)
+  player_1:draw(0, v)
 end
 
 
 function love.keypressed(key)
   if key == "escape" then
     love.event.quit()
+  end
+end
+
+function love.update(dt)
+  v = v + u
+  if u == 0 then
+    u = u - dt
+  elseif u > 1.5 then
+    u = 1.5
+    u = -(u - dt)
+  else
+    u = u + dt
+  end
+  
+  if v > 360 then
+    v = 360
   end
 end
