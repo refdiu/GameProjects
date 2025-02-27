6c60417751d43b63e811d99af3d5d95d61ad7562
refdiu <lahirishraman@gmail.com>
2025-02-27 21:12:23 +0530

    movement mostly finished, still need to crack jump logic

--- FootHeadBall/main.lua
+++ FootHeadBall/main.lua
@@ -1,15 +1,15 @@
-local v = 360
-
-local u = 1.5
+v = 360
 function love.load()  
   player_1 = require('p1')
   player_1 = P1
   	love.window.setMode(640, 480, {fullscreen = false, vsync = -1, resizable = false, centered = true, highdpi = true})
+  u = 1.5
+  s = -20
 end
 
 function love.draw()
   love.graphics.print("Hewll")
-  player_1:draw(0, v)
+  player_1:draw(s, v)
 end
 
 
@@ -21,21 +21,33 @@ end
 
 
 function love.update(dt)
+  if love.keyboard.isDown("up") then
+    u = 1.5
+  else
+    u = 0
+  end
+  
   v = v + u
-  if u == 0 then
-    u = u - dt
-  elseif u > 1.5 then
+  if u > 1.5 then
     u = 1.5
     u = -(u - dt)
   else
     u = u + dt
   end
   
-  if u < 0 then
-    u = 0
-  end
-  
   if v > 360 then
     v = 360
+  end
+  
+  if love.keyboard.isDown("left") then
+    s = s - 90*dt
+  elseif love.keyboard.isDown("right") then
+    s = s + 90*dt
+  end
+  
+  if s < -20 then
+    s = -20
+  elseif s > 600 then
+    s = 600
   end
 end
