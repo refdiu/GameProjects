4611d52248b94e3a0cb1f2a5be98750c54a7233f
refdiu <lahirishraman@gmail.com>
2025-03-03 19:25:48 +0530

    locked snake's movement so that it can't switch in opposite directions

--- Slither_in_Snake/main.lua
+++ Slither_in_Snake/main.lua
@@ -3,13 +3,15 @@ function love.load()
   snake_x = 100
   snake_y = 100
   snake_orient = 0
+  tv = {["up"] = true, ["down"] = true, ["left"] = false, ["right"] = true}
 end
-snake_yellow = love.graphics.newImage("sfhead.png")
-snake_green = love.graphics.newImage("sbhead.png")
-body = love.graphics.newImage("sbody.png")
+
+snake_yellow = love.graphics.newImage("assets/fhead.png")
+snake_green = love.graphics.newImage("assets/sbhead.png")
+body = love.graphics.newImage("assets/sbody.png")
 
 function love.draw()
-	love.graphics.draw(snake_yellow, snake_x, snake_y, snake_orient, 2, 2)
+	love.graphics.draw(snake_yellow, snake_x, snake_y, snake_orient)
   love.graphics.print(snake_x)
   love.graphics.print("\n"..snake_y)
 end
@@ -21,18 +23,24 @@ function love.keypressed(key)
 end
 
 function love.update(dt)
-  if love.keyboard.isDown("up") then
-    snake_y = snake_y - 5
+  if love.keyboard.isDown("up") and snake_orient ~= 1.57 then
     snake_orient = -1.57
-  elseif love.keyboard.isDown("down") then
-    snake_y = snake_y + 5
+  elseif love.keyboard.isDown("down") and snake_orient ~= -1.57 then
     snake_orient = 1.57
-  elseif love.keyboard.isDown("right") then
-    snake_x = snake_x + 5
+  elseif love.keyboard.isDown("right") and snake_orient ~= -3.14 then
     snake_orient = 0
-  elseif love.keyboard.isDown("left") then
-    snake_x = snake_x - 5
+  elseif love.keyboard.isDown("left") and snake_orient ~= 0 then
     snake_orient = -3.14
+  end
+  
+  if snake_orient == -1.57 then
+    snake_y = snake_y - 5
+  elseif snake_orient == 1.57 then
+    snake_y = snake_y + 5
+  elseif snake_orient == 0 then
+    snake_x = snake_x + 5
+  elseif snake_orient == -3.14 then
+    snake_x = snake_x - 5
   end
   
   if snake_x > 950 then
