fe05b5e67f53fdf74863761a9e5051f0aadfae4a
refdiu <lahirishraman@gmail.com>
2025-04-24 23:36:30 +0530

    added simulated snowfall

--- Nascent Cold/main.lua
+++ Nascent Cold/main.lua
@@ -1,3 +1,5 @@
+love.window.setMode(1366, 768, {fullscreen = true, vsync = -1, resizable = false, centered = true})
+love.window.setTitle('Nascent Cold')
 function love.load()
   u = 2
   d = 100
@@ -5,9 +7,25 @@ function love.load()
 end
 
 
+sfall = love.graphics.newImage('sf.png')
+psystem = love.graphics.newParticleSystem(sfall, 10000)
+psystem:setParticleLifetime(1, 10)
+psystem:setEmissionRate(50)
+psystem:setSizeVariation(0.89)
+psystem:setLinearAcceleration(-400, -100, 100, 500)
+psystem:setColors(1, 1, 1, 1, 1, 1, 1, 0)
+
+function love.draw()
+  love.graphics.print("hello")
+  love.graphics.draw(psystem, love.graphics.getWidth()- 140, -100, 0, 0.2, 0.2)
+end
+
 function love.keypressed(key)
   if mp_can_jump == false and key == "up" then
     mp_can_jump = true
+  end
+  if key == "escape" then
+    love.event.quit()
   end
 end
 
@@ -21,4 +39,5 @@ function love.update(dt)
       u = u + (1.5)*dt
     end
   end
+  psystem:update(dt)
 end
