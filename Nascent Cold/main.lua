04f1bcc0bd94e57f950a9f630fae6a6d43f7a874
refdiu <lahirishraman@gmail.com>
2025-05-10 21:01:30 +0530

    one bullet done. apply same logic to 6 bullets (revolver)

--- Nascent Cold/main.lua
+++ Nascent Cold/main.lua
@@ -8,13 +8,14 @@ function love.load()
   u = 2
   d = 100
   mp_can_jump = false
-  bullet = b
+  bullet = {b, b, b, b, b, b}
   mc = m_p
   bullet_count = 1
   aniform = 1
-  bx = -400
-  by = -620
+  bx = -237
+  by = -590
   can_shoot = false
+  mouse_x, mouse_y = 0
 end
 
 bg = love.graphics.newImage("assets/mpbg.png")
@@ -31,9 +32,10 @@ psystem:setColors(1, 1, 1, 1, 1, 1, 1, 0)
 function love.draw()
   love.graphics.setBackgroundColor(100, 0, 0, 200)
   --love.graphics.draw(bg)
+  love.graphics.print(mouse_x.."\n"..mouse_y)
   love.graphics.draw(psystem, love.graphics.getWidth()- 140, -100, 0, 0.2, 0.2)
   m_p:draw(100, 480, 2, 2, aniform)
-  b:draw(bx, by)
+  bullet[1]:draw(bx, by)
 end
 
 function love.keypressed(key)
@@ -51,6 +53,11 @@ function love.mousepressed(x, y, button)
   end
 end
 
+function love.mousemoved(mx, my)
+  mouse_x = mx
+  mouse_y = my
+end
+
 function love.mousereleased(x, y, button)
   if button == 2 then
     aniform = 1
@@ -61,15 +68,24 @@ function love.update(dt)
   if love.mouse.isDown(2) and love.mouse.isDown(1) then
     aniform = 3
     can_shoot = true
+    if by < 0 then
+      by = -by
+    end
+    if bx < 0 then
+      bx = -bx
+    end
+    bullet_shot()
   end
   
   if can_shoot then
-    bx = b:traverse(bx, dt)
+    bx = bullet[1]:traverse(bx, dt)
   end
   
-  if bx >= 1366 then
-    can_shoot = false
-    bx = 400
+  function bullet_shot()
+    if bx >= 1366 then
+      can_shoot = false
+      bx = -237
+    end
   end
   psystem:update(dt)
 end
