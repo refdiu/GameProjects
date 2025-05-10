5618f9d1d2abd1c2931d2bed582eb327a404bc72
refdiu <lahirishraman@gmail.com>
2025-05-10 23:34:40 +0530

    need the body to trail behind the head instead of immediately turning in that direction.

--- Slither_in_Snake/main.lua
+++ Slither_in_Snake/main.lua
@@ -9,27 +9,35 @@ function love.load()
   counter = 0
   mouse_x = 0
   mouse_y = 0
+  next_x = 0
+  next_y = 0
 end
 
 snake_yellow = love.graphics.newImage("assets/sfhead.png")
+snake_body = love.graphics.newImage("assets/sbody.png")
 snake_green = love.graphics.newImage("assets/sbhead.png")
-snake_table = {snake_green, snake_green}
+snake_table = {snake_yellow}
 pellet = love.graphics.newImage("assets/pellet.png")
 
 function love.draw()
-	love.graphics.draw(pellet, rand_x, rand_y, 0, 2, 2)  
+	love.graphics.draw(pellet, rand_x, rand_y, 0, 2, 2)
+  love.graphics.draw(snake_table[1], snake_x, snake_y, snake_orient, 1, 1, snake_green:getWidth()/2, snake_green:getHeight()/2)
+  
   for i, j in ipairs(snake_table) do
-    if snake_orient == -1.57 then
-      love.graphics.draw(snake_table[i], snake_x, snake_y -(snake_green:getHeight()*(i-1)), snake_orient, 1, 1, snake_green:getWidth()/2, snake_green:getHeight()/2)
-    elseif snake_orient == 1.57 then
-      love.graphics.draw(snake_table[i], snake_x, snake_y +(snake_green:getHeight()*(i-1)), snake_orient, 1, 1, snake_green:getWidth()/2, snake_green:getHeight()/2)
-    elseif snake_orient == 0 then
-      love.graphics.draw(snake_table[i], snake_x-(snake_green:getWidth()*(i-1)), snake_y, snake_orient, 1, 1, snake_green:getWidth()/2, snake_green:getHeight()/2)
-    elseif snake_orient == -3.14 then
-      love.graphics.draw(snake_table[i], snake_x+(snake_green:getWidth()*(i-1)), snake_y, snake_orient, 1, 1, snake_green:getWidth()/2, snake_green:getHeight()/2)
+    if counter > 0 then
+      if snake_orient == -1.57 then
+        love.graphics.draw(snake_table[i], snake_x, snake_y -(snake_green:getHeight()*(i-1)), snake_orient, 1, 1, snake_green:getWidth()/2, snake_green:getHeight()/2)
+      elseif snake_orient == 1.57 then
+        love.graphics.draw(snake_table[i], snake_x, snake_y +(snake_green:getHeight()*(i-1)), snake_orient, 1, 1, snake_green:getWidth()/2, snake_green:getHeight()/2)
+      elseif snake_orient == 0 then
+        love.graphics.draw(snake_table[i], snake_x-(snake_green:getWidth()*(i-1)), snake_y, snake_orient, 1, 1, snake_green:getWidth()/2, snake_green:getHeight()/2)
+      elseif snake_orient == -3.14 then
+        love.graphics.draw(snake_table[i], snake_x+(snake_green:getWidth()*(i-1)), snake_y, snake_orient, 1, 1, snake_green:getWidth()/2, snake_green:getHeight()/2)
+      end
     end
-    
   end
+  
+  
   love.graphics.print(counter)
   love.graphics.print("\n"..mouse_x)
   love.graphics.print("\n\n"..mouse_y)
@@ -59,20 +67,20 @@ function love.update(dt)
   
   
   if snake_orient == -1.57 then
-    snake_y = snake_y - 3
+    snake_y = snake_y - 1
   elseif snake_orient == 1.57 then
-    snake_y = snake_y + 3
+    snake_y = snake_y + 1
   elseif snake_orient == 0 then
-    snake_x = snake_x + 3
+    snake_x = snake_x + 1
   elseif snake_orient == -3.14 then
-    snake_x = snake_x - 3
+    snake_x = snake_x - 1
   end
   
   if snake_x >= rand_x-30 and snake_x <= rand_x+20 and snake_y >= rand_y-30 and snake_y <= rand_y+20 then
     counter = counter + 1
     rand_x = math.random(0, 958)
     rand_y = math.random(0, 358)
-    table.insert(snake_table, snake_green)
+    table.insert(snake_table, snake_body)
   end
   
   if snake_x <= 0 or snake_x >= 950 or snake_y <= 0 or snake_y >= 350 then
