a4fa789b788eff34f43e82e796da82dee9c79b5a
refdiu <lahirishraman@gmail.com>
2025-05-11 15:21:52 +0530

    interesting new awareness: made this harder for myself by not using grid

--- Slither_in_Snake/main.lua
+++ Slither_in_Snake/main.lua
@@ -1,16 +1,15 @@
+snake_x = 100
+snake_y = 100
 function love.load()
   love.window.setMode(960, 360, {fullscreen = false, vsync = -1, resizable = false, centered = true, highdpi = true})
-  snake_x = 100
-  snake_y = 100
   --love.graphics.newImage("assets/sbody.png")
   snake_orient = 0
   rand_x = math.random(0, 959)
   rand_y = math.random(0, 359)
   counter = 0
-  mouse_x = 0
-  mouse_y = 0
-  next_x = 0
-  next_y = 0
+  mouse_x, mouse_y = 0
+  next_x, next_y = 0
+  last_x, last_y = 0
 end
 
 snake_yellow = love.graphics.newImage("assets/sfhead.png")
@@ -24,16 +23,9 @@ function love.draw()
   love.graphics.draw(snake_table[1], snake_x, snake_y, snake_orient, 1, 1, snake_green:getWidth()/2, snake_green:getHeight()/2)
   
   for i, j in ipairs(snake_table) do
-    if counter > 0 then
-      if snake_orient == -1.57 then
-        love.graphics.draw(snake_table[i], snake_x, snake_y -(snake_green:getHeight()*(i-1)), snake_orient, 1, 1, snake_green:getWidth()/2, snake_green:getHeight()/2)
-      elseif snake_orient == 1.57 then
-        love.graphics.draw(snake_table[i], snake_x, snake_y +(snake_green:getHeight()*(i-1)), snake_orient, 1, 1, snake_green:getWidth()/2, snake_green:getHeight()/2)
-      elseif snake_orient == 0 then
-        love.graphics.draw(snake_table[i], snake_x-(snake_green:getWidth()*(i-1)), snake_y, snake_orient, 1, 1, snake_green:getWidth()/2, snake_green:getHeight()/2)
-      elseif snake_orient == -3.14 then
-        love.graphics.draw(snake_table[i], snake_x+(snake_green:getWidth()*(i-1)), snake_y, snake_orient, 1, 1, snake_green:getWidth()/2, snake_green:getHeight()/2)
-      end
+    if counter > 0 and i > 2 then
+      love.graphics.draw(snake_table[i], last_x, last_y, snake_orient, 1, 1, snake_green:getWidth()/2, snake_green:getHeight()/2)
+      last_x, last_y = next_x, next_y
     end
   end
   
@@ -67,13 +59,13 @@ function love.update(dt)
   
   
   if snake_orient == -1.57 then
-    snake_y = snake_y - 1
+    snake_y = snake_y - 3
   elseif snake_orient == 1.57 then
-    snake_y = snake_y + 1
+    snake_y = snake_y + 3
   elseif snake_orient == 0 then
-    snake_x = snake_x + 1
+    snake_x = snake_x + 3
   elseif snake_orient == -3.14 then
-    snake_x = snake_x - 1
+    snake_x = snake_x - 3
   end
   
   if snake_x >= rand_x-30 and snake_x <= rand_x+20 and snake_y >= rand_y-30 and snake_y <= rand_y+20 then
@@ -81,6 +73,7 @@ function love.update(dt)
     rand_x = math.random(0, 958)
     rand_y = math.random(0, 358)
     table.insert(snake_table, snake_body)
+    next_x, next_y = snake_x - snake_yellow:getWidth(), snake_y - snake_yellow:getHeight()
   end
   
   if snake_x <= 0 or snake_x >= 950 or snake_y <= 0 or snake_y >= 350 then
