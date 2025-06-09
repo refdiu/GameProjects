58c7b73831333514fe14a33af1c0f51726c94c9a
refdiu <lahirishraman@gmail.com>
2025-06-09 15:32:48 +0530

    newgame, a chrome dino clone

--- dinoclone/main.lua
+++ dinoclone/main.lua
@@ -1,116 +1,21 @@
 	--some presets
-love.window.setMode(1366, 768, {fullscreen = false, vsync = -1, resizable = false, centered = true})
+love.window.setMode(700, 300, {fullscreen = false, vsync = -1, resizable = false, centered = true})
 love.window.setTitle('Oh my Gotto boy')
+require("t")
+t = trees
+background = love.graphics.newImage("Background.png")
 
-
---dependencies
-require('assets/world/back_g')
-require('assets/world/coo')
-wbg = love.graphics.newImage("assets/world/world_tileset.png")
-sfall = love.graphics.newImage("assets/world/sf.png")
-
---the "camera"
-local cam_abs = 0
-local cam_ord = 340
-local cam_xspeed = 120
-local cam_dyspeed = 0
-jump_v = -70
-g = 4
-
-
---some required constants
-i = 1
-tiles = {}
-gentiles = {33, 15, 23}
-
---background inclusion
-bg = Background
-bg:render(1)
-
---de-bloats some conditional statements
-f = Features
-
---the real assets
-player = love.graphics.newImage("assets/world/player.png")
-p_height = love.graphics.getWidth(player)
-tramp = love.graphics.newImage("assets/world/trampoline.png")
-pd = love.graphics.newImage("assets/world/pd_sprite.png")
-
---tilemaps
-l1t = love.graphics.newQuad(0, 160, 16, 16, wbg)
---sfall = love.graphics.newQuad(0, 144, 16, 16, wbg)
-
---particle system
-psystem = love.graphics.newParticleSystem(sfall, 1000)
-psystem:setParticleLifetime(1, 10) -- Particles live at least 2s and at most 5s.
-psystem:setEmissionRate(50)
-psystem:setSizeVariation(0.13)
-psystem:setLinearAcceleration(-400, -100, 100, 500) -- Random movement in all directions.
-psystem:setColors(1, 1, 1, 1, 1, 1, 1, 0) -- Fade to transparency
-
-
-for x = 1, 40 do
-	table.insert(tiles, {})
-	for y = 1, 6 do
-		table.insert(tiles[x], l1t)
-	end
+function love.load()
+  tx = 701
 end
 
-
---key config
-function love.keypressed(key)
-	if key == 'escape' then
-		love.event.quit()
-	end
-	if key == 'r' then
-		dofile("main.lua")
-	end
-end
-
-
---the real thing happens here
 function love.draw()
-	bg:drawim()
-	love.graphics.draw(psystem, love.graphics.getWidth() + 40, 0)
-	love.graphics.draw(player, 100, cam_ord, 0, 0.7, 0.7)
-	love.graphics.translate(-math.floor(cam_abs), 0)
-	for x = 1, 40 do
-		if x == gentiles[1] or x == gentiles[2] or x == gentiles[3] then
-			goto continue
-		else
-			for y = 1, 6 do
-				love.graphics.draw(wbg, tiles[x][y], 16*(x-1), 384 + 16*(y-1))
-			end
-		end
-		::continue::
-	end
+  t:draw(tx)
 end
 
-
-function love.keypressed(key)
-	if key == "up" and cam_dyspeed == 0 then
-		cam_dyspeed = jump_v
-	end
-end
-
-function love.mousemoved(x, y)
-	mx = x
-	my = y
-end
-
-
---updation
 function love.update(dt)
-	psystem:update(dt)
-	cam_dyspeed = cam_dyspeed + g
-	cam_ord = cam_ord + cam_dyspeed*dt
-	if cam_ord > 340 then
-		cam_ord = 340
-		cam_dyspeed = 0
-	end
-	if love.keyboard.isDown("left") then
-		cam_abs = cam_abs - cam_xspeed*dt
-	elseif love.keyboard.isDown("right") then
-		cam_abs = cam_abs + cam_xspeed*dt
-	end
+  tx = tx - 90*dt
+  if tx < 0 then
+    tx = 701
+  end
 end
---
+++ dinoclone/t.lua
@@ -0,0 +1,13 @@
+tsprite = love.graphics.newImage("strees.png")
+trees = {t1 = love.graphics.newQuad(48, 0, 16, 32, tsprite), t2 = love.graphics.newQuad(48, 32, 16, 32, tsprite)}
+
+function trees:new()
+  o = o or {}
+	setmetatable(o, self)
+	self.__index = self
+	return o
+end
+
+function trees:draw(tx)
+  love.graphics.draw(tsprite, self.t1, tx, 140, 0, 4, 4)
+end
